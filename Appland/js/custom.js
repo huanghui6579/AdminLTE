df81485b76ab0150550b9b6a1c80eb40ddd0bd3e
huanghui6579 <huanghui6579@163.com>
2016-11-06 17:13:49 +0800

    完善了更新日志的界面

--- Appland/js/custom.js
+++ Appland/js/custom.js
@@ -67,7 +67,7 @@
 	});
 
 	//nivo lightbox
-	$('.gallery-item a').nivoLightbox({
+	/*$('.gallery-item a').nivoLightbox({
 		effect: 'fadeScale',                             // The effect to use when showing the lightbox
 		theme: 'default',                           // The lightbox theme to use
 		keyboardNav: true,                          // Enable/Disable keyboard navigation (left/right/escape)
@@ -80,7 +80,7 @@
 		onPrev: function(element){},                // Callback when the lightbox gallery goes to previous item
 		onNext: function(element){},                // Callback when the lightbox gallery goes to next item
 		errorMessage: 'The requested content cannot be loaded. Please try again later.' // Error message when content can't be loaded
-	});
+	});*/
 	
 })(jQuery);
 $(window).load(function() {
